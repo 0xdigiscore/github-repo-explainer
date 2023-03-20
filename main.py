ac9ab5e7c3a02f489bde1175d65cd0cd2985138b
0xhelloweb3 <1245816264@qq.com>
2023-03-20 18:23:41 +0800

    feat: no need download each file

--- main.py
+++ main.py
@@ -9,32 +9,33 @@ from utils.file_utils import create_directory_if_not_exists
 def main(user, repo, branch):
     download_repo_zip(user, repo, branch)
 
-    contents = get_repo_contents_recursive(user, repo)
-    code_files = [item for item in contents if item["name"].endswith(".py")]
+    files = []
+    for subdir, _, files_list in os.walk(repo):
+        for file in files_list:
+            if file.endswith(".py"):
+                files.append(os.path.join(subdir, file))
 
-    if not code_files:
+    if not files:
         print(f"No Python files found in the {user}/{repo} repository.")
         return
 
     output_directory = f"{user}_{repo}_md_explanations"
     create_directory_if_not_exists(output_directory)
 
-    for code_file in code_files:
-        file_url = code_file["download_url"]
-        file_name = code_file["name"]
+    for code_file in files:
+        file_name = os.path.basename(code_file)
+        file_url = f"file://{os.path.abspath(code_file)}"
         destination_path = os.path.join(output_directory, f"{os.path.splitext(file_name)[0]}.md")
 
-        download_file(file_url, file_name)
-        file_explanation = get_file_explanation(file_name)
+        file_explanation = get_file_explanation(code_file)
         with open(destination_path, "w") as f:
             f.write(file_explanation)
-
-        os.remove(file_name)
 
         print(f"File {file_name} processed and explanation written to {destination_path}.")
 
     print(f"All Python files in {user}/{repo} processed successfully.")
-        
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python main.py <github_username> <repository_name>")
